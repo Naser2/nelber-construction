8371df0497f422cca5d59ac4e6efa2926d0b7746
Wambani01 <georgewmbn@gmail.com>
2025-02-12 21:49:56 +0300

    commit hero

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Image from 'next/image';
+
+export default function Hero() {
+    return (
+        <div>
+    
+                    <Image
+                      className="dark:invert"
+                      src="/next.svg"
+                      alt="Next.js logo"
+                      width={180}
+                      height={38}
+                      priority
+                    />
+            <h1>hero</h1>
+        </div>
+    );
+}
